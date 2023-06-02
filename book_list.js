35c3313c46c1e1b3bda60ddd02ed03269fba84f1
Jenny Chen <jenny.chenchou@gmail.com>
2023-06-02 22:37:38 +0200

    add alert for warning/successfully added book/ remove book

--- book_list.js
+++ book_list.js
@@ -41,6 +41,18 @@ class UI {
       el.parentElement.parentElement.remove();
     }
   }
+
+  static showAlert(message, className) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#book-form");
+    container.insertBefore(div, form);
+    // vanish in 3 seconds
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+  }
+
   static clearInput() {
     document.querySelector("#title").value = "";
     document.querySelector("#author").value = "";
@@ -60,18 +72,27 @@ document.querySelector("#book-form").addEventListener("submit", (e) => {
   const author = document.querySelector("#author").value;
   const isbn = document.querySelector("#isbn").value;
 
-  // Instatiate book
-  const book = new Book(title, author, isbn);
+  // Validation
+  if(title === "" || author === "" || isbn === "") {
+    UI.showAlert("Please fill in all fields!", "warning")
+  } else {  // Instatiate book
+    const book = new Book(title, author, isbn);
+  
+    const row = document.createElement("tr");
+  
+    // Add book to list
+    UI.addBookToList(book);
 
-  const row = document.createElement("tr");
-
-  // Add book to list
-  UI.addBookToList(book);
-
-  // Clear fields
-  UI.clearInput();
+    // Show success message
+    UI.showAlert("Book Added", "success")
+  
+    // Clear fields
+    UI.clearInput();}
 });
 
 document.querySelector("#book-list").addEventListener("click", (e) => {
   UI.deleteBook(e.target);
+
+    // Show delete message
+    UI.showAlert("Book Removed", "danger")
 });
